2df8bb2da795c7ac0fc63008e85262bd29fc3de3
Ubuntu <ubuntu@ip-172-31-13-222.eu-west-1.compute.internal>
2020-07-09 22:19:21 +0000

    venv made

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from application import app
+from flask import Flask, render_template, request, url_for, redirect
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run('0.0.0.0', debug=)
+    app.run('0.0.0.0', debug=True)
+
