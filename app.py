ecc50df99738276393655c0dee7d605074b6899b
Ubuntu <ubuntu@ip-117-60-1-9.eu-west-1.compute.internal>
2020-07-13 14:07:04 +0000

    monday progress

--- app.py
+++ app.py
@@ -6,9 +6,21 @@ import csv
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        details = request.form
+        player_result  = details['p_league1']
+        team_result = details['t_league1']
+        print(player_result + " " +  team_result)
+        if player_result == 'true':
+            with open('application/textfiles/players.txt') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                for row in csv_reader:
+                    player = str(row[random.randint(0,5)])
+                    print(player)
+                    
+    else:
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run('0.0.0.0', debug=True)
